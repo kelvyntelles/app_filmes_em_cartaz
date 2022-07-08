c404d43d2b0b7a4cbe14710210b46971e67d2ebe
Kelvyn Telles <kelvyn.telles@gmail.com>
2022-07-08 14:49:47 -0300

    projeto concluido

--- App.js
+++ App.js
@@ -1,20 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from "react";
+import { SafeAreaView, FlatList, Image, Button, Text, View, StyleSheet, ActivityIndicator } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const requestMovies = async () => {
+      setLoading(true);
+      const res = await fetch("https://api.b7web.com.br/cinema/");
+      const json = await res.json();
+
+      if(json) {
+        setMovies(json);
+      }
+
+      setLoading(false);
+    }
+
+    requestMovies();
+  }, [])
+
+  return(
+    <SafeAreaView style={styles.container}>
+      {loading &&
+        <View style={styles.loadingArea}>
+          <ActivityIndicator size="large" color="#FFFFFF" />
+          <Text style={styles.loadingText}>Carregando...</Text>
+        </View>
+      }
+      {!loading &&
+        <>
+          <Text style={styles.totalMoviesText}>Total de Filmes: {movies.length}</Text>
+            <FlatList style={styles.list}
+              data={movies}
+              renderItem={({item})=>(
+                <View style={styles.movieItem}>
+                  <Image 
+                    source={{uri: item.avatar}} 
+                    style={styles.movieImage} 
+                    resizeMode="contain"
+                  />
+                  <Text style={styles.movieTitle}>{item.titulo}</Text>
+                </View>
+              )}
+              keyExtractor={item => item.titulo}
+            /> 
+        </>
+      }
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 30,
+    backgroundColor: '#333'
   },
-});
+  loadingArea: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loadingText: {
+    color: '#FFF',
+  },
+  totalMoviesText: {
+    color: '#FFF',
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 10
+  },
+  list: {
+    flex: 1,
+  },
+  movieItem: {
+    marginBottom: 30
+  },
+  movieImage: {
+    height: 400
+  },
+  movieTitle: {
+    color: '#FFF',
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 5
+  }
+})
+
+export default App;
+
